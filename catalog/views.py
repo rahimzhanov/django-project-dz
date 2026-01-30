84fa988f82a32f7863b7bb0f435f84156e9ef665
Rakhimzhanov Aman <amanrahim2@gmail.com>
2026-01-30 22:28:48 +0500

    add forms

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, ListView, DetailView
-from django.views import View
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy, reverse
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from catalog.models import Product, Category
-
+from catalog.forms import ProductForm
 
 class HomeView(ListView):
     model = Product
@@ -31,3 +33,34 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        # Можно добавить автоматическое заполнение полей
+        return super().form_valid(form)
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Удаление продукта'
+        return context
--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('blogs/', include('blog.urls', namespace='blog')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
